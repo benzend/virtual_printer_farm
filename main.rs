4c4ee9357536e96ca4a5b09d8d11e703c9b34721
Benjamin <benjamin.scottt.dev@gmail.com>
2023-02-14 21:00:20 -0700

    chore: use clap and init first cmd (#6)

--- main.rs
+++ main.rs
@@ -1,2 +1,27 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    port: Option<i64>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Create,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Create) => {
+            println!("printer created");
+        },
+        _ => panic!("faulty command")
+    }
 }
