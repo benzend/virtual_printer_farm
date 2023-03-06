38527e6219fa5b63d793f3fe432ccdc4a5b50e54
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-03-05 21:29:08 -0700

    refactor[ctx]: add ctx to handle client & cli refs

--- src/main.rs
+++ src/main.rs
@@ -17,30 +17,47 @@ enum Commands {
     Create,
 }
 
+struct Ctx {
+    db_client: Client,
+    cli: Cli,
+}
+
+impl Ctx {
+    fn new(db_client: Client, cli: Cli) -> Ctx {
+        Ctx { db_client, cli }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
-    let mut client = match setup_db_client() {
+    let client = match setup_db_client() {
         Ok(client) => client,
         Err(e) => panic!("{}", e.to_string())
     };
 
-    let port = match &cli.port {
-        Some(p) => p,
-        None => &(3000 as i64)
+    let mut ctx = Ctx::new(client, cli);
+
+    let port = match &ctx.cli.port {
+        Some(p) => *p,
+        None => 3000 as i64
     };
 
-    match &cli.command {
+    match &ctx.cli.command {
         Some(Commands::Create) => {
-            client.execute(
-                "INSERT INTO printer (port) VALUE ($1)",
-                &[&port]
-            )?;
-            println!("printer created");
-            Ok(())
+            handle_create_printer(&port, &mut ctx)
         },
         _ => panic!("not a valid command")
     }
+}
+
+fn handle_create_printer(port: &i64, ctx: &mut Ctx) -> Result<(), Box<dyn std::error::Error>> {
+    ctx.db_client.execute(
+        "INSERT INTO printer (port) VALUE ($1)",
+        &[port]
+    )?;
+    println!("printer created");
+    Ok(())
 }
 
 fn setup_db_client() -> Result<Client, Box<dyn std::error::Error>> {
